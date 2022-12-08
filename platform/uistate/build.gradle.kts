8483a27364f6f145f0114e9fb3b07ffe8e13fa26
lucca <lbeurmann.dev@gmail.com>
2022-12-08 20:58:30 -0300

    config: add missing compose configuration in platform module

--- platform/uistate/build.gradle.kts
+++ platform/uistate/build.gradle.kts
@@ -32,6 +32,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
 }
 
 dependencies {
