a620f9b1a950785f825f6ffa90a92e3d03a54482
lucca <lbeurmann.dev@gmail.com>
2022-12-21 21:03:04 -0300

    awdawd

--- app/src/main/java/com/example/mvisample/MainActivity.kt
+++ app/src/main/java/com/example/mvisample/MainActivity.kt
@@ -1,31 +1,34 @@
 package com.example.mvisample
 
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.mvisample.ui.theme.MviSampleTheme
-import com.example.uievent.UiEventEffect
-import com.example.uistate.UiStateContent
 
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
@@ -33,59 +36,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MviSampleTheme {
-                val viewModel by viewModels<MainViewModel>(
-                    factoryProducer = { MainViewModelProvider() }
-                )
-                val context = LocalContext.current
-                val state by viewModel.collectUiState()
-
-                UiEventEffect(
-                    eventHandler = viewModel,
-                    onEventReceived = {
-                        Toast.makeText(
-                            context, it.name,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                )
-
-                state.UiStateContent(
-                    stateContent = {
-                        MainView(
-                            state = it,
-                            handleIntent = viewModel::handleIntent
-                        )
-                    },
-                    errorState = {
-
-                    }
-                )
-            }
-        }
-    }
-
-    @Composable
-    internal fun MainView(
-        state: MainState,
-        handleIntent: (MainIntent) -> Unit
-    ) {
-        Scaffold {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Button(onClick = { handleIntent(MainIntent.SendEvent) }) {
-                    Text("Send UI event")
+                val list = remember {
+                    mutableStateOf(listOf<Uri>())
                 }
-                Spacer(Modifier.height(16.dp))
-                OutlinedTextField(
-                    value = state.mock,
-                    onValueChange = { text -> handleIntent(MainIntent.UpdateText(text)) },
-                    modifier = Modifier.fillMaxWidth()
+                val context = LocalContext.current
 
-                )
+                val picker =
+                    rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.PickVisualMedia(),
+                        onResult = { Uri ->
+                            Uri?.let { nullSafeUri ->
+                                list.value = list.value + nullSafeUri
+                                Log.d("image", nullSafeUri.toString())
+                                Log.d("images", list.value.toString())
+                            }
+                        }
+                    )
+
+                Scaffold() {
+                    Column(Modifier.padding(it)) {
+                        Button(
+                            onClick = {
+                                picker.launch(
+                                    PickVisualMediaRequest(
+                                        ActivityResultContracts.PickVisualMedia.ImageOnly
+                                    )
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text("Open image pcker")
+                        }
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(3),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(it),
+                            horizontalArrangement = Arrangement.spacedBy(5.dp),
+                            verticalArrangement = Arrangement.spacedBy(5.dp)
+                        ) {
+                            items(list.value) { imageUri ->
+                                AsyncImage(
+                                    model = ImageRequest.Builder(context)
+                                        .data(imageUri)
+                                        .build(),
+                                    contentDescription = null,
+                                    modifier = Modifier.height(150.dp),
+                                    contentScale = ContentScale.Crop,
+                                )
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
