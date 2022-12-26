82a95a9eaefd5ebf22115f56c0ed10d778e946e8
lucca <lbeurmann.dev@gmail.com>
2022-12-26 17:40:39 -0300

    test: add tests to runQuery

---
+++ database/models/src/test/kotlin/database/models/utils/RunQueryKtTest.kt
@@ -0,0 +1,32 @@
+package database.models.utils
+
+import com.example.error.InternalException
+import com.google.common.truth.Truth
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+internal class RunQueryKtTest {
+
+    @Test(expected = InternalException::class)
+    fun `Should throw internal exception when Generic type is captured`() = runTest {
+        runQuery {
+            throw InternalException.Generic("aaa")
+        }
+    }
+
+    @Test
+    fun `Should propagate unhandled exception when Throwable is captured`() = runTest {
+        try {
+            runQuery {
+                throw IllegalArgumentException()
+            }
+        } catch (e: Throwable) {
+            Truth.assertThat(e).isInstanceOf(InternalException.Untreated::class.java)
+            Truth.assertThat((e as InternalException.Untreated).originalError)
+                .isInstanceOf(IllegalArgumentException::class.java)
+        }
+    }
+
+}
