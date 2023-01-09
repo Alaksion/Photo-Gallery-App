820f2d22304fe92f058f58cb2501538a0fb6f676
lucca <lbeurmann.dev@gmail.com>
2023-01-09 16:46:02 -0300

    doc: add Kdocs to logging classes

--- platform/logs/src/main/kotlin/platform/logs/LogProvider.kt
+++ platform/logs/src/main/kotlin/platform/logs/LogProvider.kt
@@ -4,6 +4,9 @@ import platform.logs.loggers.AppLogger
 import platform.logs.loggers.DebugLogger
 import platform.logs.loggers.SilentLogger
 
+/**
+ * Provides an instance of [AppLogger] depending on the currently active build variant.
+ * */
 internal object LogProvider {
 
     fun provide(): AppLogger = if (BuildConfig.DEBUG) DebugLogger() else SilentLogger()
--- platform/logs/src/main/kotlin/platform/logs/loggers/AppLogger.kt
+++ platform/logs/src/main/kotlin/platform/logs/loggers/AppLogger.kt
@@ -4,11 +4,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import platform.logs.LogProvider
 
+typealias LogTag = String
+
+/**
+ * Interface responsible for registering local logs. Use this class to help identifying important
+ * actions on Logcat.
+ * */
 interface AppLogger {
 
+    /**
+     * Register a log entry in Log cat.
+     * @param type Type of the log. Can be of types [LogType.Debug], [LogType.Error], [LogType.Warning]
+     * @param logTag Identifier of the log entry, search for this tag in log cat to easily find
+     * occurrences of a given log.
+     * @param content Content to be logged.
+     * */
     fun registerLog(
         type: LogType,
-        logTag: String,
+        logTag: LogTag,
         content: String
     )
 
@@ -20,5 +33,8 @@ enum class LogType {
     Warning,
 }
 
+/**
+ * Convenience function to access instances of [AppLogger] inside @Composable annotated scopes.
+ * */
 @Composable
 fun rememberAppLogger() = remember { LogProvider.provide() }
