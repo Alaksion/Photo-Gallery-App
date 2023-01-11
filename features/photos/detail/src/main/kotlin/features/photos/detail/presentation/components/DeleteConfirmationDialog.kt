72d30ec65aa83e93624c66a345a0becd232648f2
lucca <lbeurmann.dev@gmail.com>
2023-01-11 18:59:09 -0300

    arch: extract bottomsheet to isolated component

--- features/photos/detail/src/main/kotlin/features/photos/detail/presentation/components/DeleteConfirmationDialog.kt
+++ features/photos/detail/src/main/kotlin/features/photos/detail/presentation/components/DeleteConfirmationDialog.kt
@@ -1,24 +1,15 @@
 package features.photos.detail.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import platform.uicomponents.MviBottomSheet
 import platform.uicomponents.MviSampleSizes
 import platform.uicomponents.components.spacers.VerticalSpacer
 
@@ -28,23 +19,10 @@ internal fun DeleteConfirmationDialog(
     onClickConfirm: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.padding(all = MviSampleSizes.medium)
+    MviBottomSheet(
+        title = "Deleting photo",
+        modifier = modifier,
     ) {
-        Box(
-            modifier = Modifier
-                .clip(MaterialTheme.shapes.large)
-                .width(MviSampleSizes.xLarge3)
-                .height(MviSampleSizes.xSmall2)
-                .background(Color.LightGray)
-                .align(CenterHorizontally)
-        )
-        VerticalSpacer(height = MviSampleSizes.medium)
-        Text(
-            text = "Deleting photo",
-            style = MaterialTheme.typography.headlineSmall
-        )
-        VerticalSpacer(height = MviSampleSizes.medium)
         Text(
             text = "This photo will be permanently removed from this album. But you can add it back" +
                     "since this action will not delete the photo from your gallery."
