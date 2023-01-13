aac1032f6cb0d25a6e780e3e9e071af8af7193d5
Lucca Beurmann <lbeurmann.dev@gmail.com>
2023-01-13 09:54:55 -0300

    feat: add pull refresh to home

--- features/albums/home/src/main/kotlin/features/albums/home/presentation/HomeState.kt
+++ features/albums/home/src/main/kotlin/features/albums/home/presentation/HomeState.kt
@@ -4,5 +4,6 @@ import platform.database.models.models.album.AlbumModel
 
 internal data class HomeState(
     val isInitialized: Boolean = false,
+    val isRefreshing: Boolean = false,
     val albums: List<AlbumModel> = listOf(),
 )
